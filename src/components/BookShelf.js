f4e6f25a49a4d32ab92a09afca73a8d2f13db63b
mindeleven <mindeleven@posteo.de>
2021-12-20 12:25:36 +0100

    Changed button to react-router Link component

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ShelfCompartment from './ShelfCompartment';
+import { Link } from "react-router-dom";
 
 const BookShelf = props => {
   const compartments = {
@@ -60,6 +61,7 @@ const BookShelf = props => {
       <div className="list-books-title">
         <h1>MyReads</h1>
       </div>
+
       <div className="list-books-content">
         <div>
           <ShelfCompartment
@@ -86,7 +88,7 @@ const BookShelf = props => {
         </div>
       </div>
       <div className="open-search">
-        <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+        <Link to="/search">Add a book</Link>
       </div>
     </div>
   )
