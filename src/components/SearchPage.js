e19cd70fb336a47a3902fe2cacd9ca5ac10aee77
mindeleven <mindeleven@posteo.de>
2021-12-21 12:13:52 +0100

    Passing down the state of the already listed books to the search

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -10,12 +10,25 @@ class SearchPage extends React.Component {
 
   searchBooks = event => {
     const query = event.target.value;
+    const booksOnShelf = this.props.books;
+    //console.log(booksOnShelf);
 
     if (query) {
       BooksAPI.search(query).then(books => {
-        books.length > 0
-          ? this.setState({ books: books })
-          : this.setState({ books: [] });
+        if (books.length > 0) {
+          books.forEach((book, index) => {
+            //console.log(books[index].id);
+            const listedBook = booksOnShelf.find(
+              elem => elem.id === book.id
+            );
+            listedBook
+              ? book.shelf = listedBook.shelf
+              : book.shelf = 'none';
+          })
+          this.setState({ books: books })
+        } else {
+          this.setState({ books: [] });
+        }
       });
     } else {
       this.setState({ books: [] });
@@ -23,7 +36,10 @@ class SearchPage extends React.Component {
   };
 
   render() {
-    console.log(this.state.books.length)
+    const { books } = this.state;
+
+    //console.log('books found: ', books.length);
+    //console.log(books)
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -51,14 +67,14 @@ class SearchPage extends React.Component {
           </div>
         </div>
         <div className="search-books-results">
-          {this.state.books.length > 0 && (
+          {books.length > 0 && (
           <ol className="books-grid">
-            {this.state.books.map((book, index) => (
+            {books.map((book, index) => (
               <Book
                 key={index}
                 book={book}
                 changeShelf={this.props.changeShelf}
-                currentShelf='none'
+                currentShelf={book.shelf}
               />
             ))}
           </ol>
